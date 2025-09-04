4499fde0c4657672513e0fdd66e2a65e531ecc35
Ch-M-Abdullah <kabeerhussain692@gmail.com>
2025-09-04 13:22:18 +0500

    added a try-catch block

--- db/config/createTables.ts
+++ db/config/createTables.ts
@@ -1,34 +1,38 @@
 import { SQLiteDatabase } from "expo-sqlite";
 
 export default async function createTables(database: SQLiteDatabase) {
-  // Create topic table
-  await database.execAsync(`
-    CREATE TABLE IF NOT EXISTS topic (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL
-    );
-  `);
+  try {
+    // Create topic table
+    await database.execAsync(`
+      CREATE TABLE IF NOT EXISTS topic (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+      );
+    `);
 
-  // Create sub_topics table with foreign key constraint
-  await database.execAsync(`
-    CREATE TABLE IF NOT EXISTS sub_topic (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      topic_id INTEGER NOT NULL,
-      done INTEGER NOT NULL DEFAULT 0,
-      FOREIGN KEY(topic_id) REFERENCES topic(id) ON DELETE CASCADE
-    );
-  `);
+    // Create sub_topics table with foreign key constraint
+    await database.execAsync(`
+      CREATE TABLE IF NOT EXISTS sub_topic (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        topic_id INTEGER NOT NULL,
+        done INTEGER NOT NULL DEFAULT 0,
+        FOREIGN KEY(topic_id) REFERENCES topic(id) ON DELETE CASCADE
+      );
+    `);
 
-  // Create resources table
-  await database.execAsync(`
-        CREATE TABLE IF NOT EXISTS resource (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            sub_topic_id INTEGER NOT NULL,
-            link TEXT,
-            type TEXT,
-            FOREIGN KEY(sub_topic_id) REFERENCES sub_topic(id) ON DELETE CASCADE
-    );
-  `);
+    // Create resources table
+    await database.execAsync(`
+          CREATE TABLE IF NOT EXISTS resource (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL,
+              sub_topic_id INTEGER NOT NULL,
+              link TEXT,
+              type TEXT,
+              FOREIGN KEY(sub_topic_id) REFERENCES sub_topic(id) ON DELETE CASCADE
+      );
+    `);
+  } catch (err: any) {
+    throw err;
+  }
 }
